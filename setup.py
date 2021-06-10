da74f73b89476ce507fb3f554a1efb0d9b12736f
Thomas Dahl Heshe <tdh@veo.co>
2021-06-10 11:24:17 +0200

    some initial CI

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name="src",
     packages=find_packages(),
-    version='0.1.0',
-    description='Recreating colors in broken images using Kornia framework',
-    author='Thomas Heshe, Asger Græsholt and Morten Wehlast',
-    license='MIT',
+    version="0.1.0",
+    description="Recreating colors in broken images using Kornia framework",
+    author="Thomas Heshe, Asger Græsholt and Morten Wehlast",
+    license="MIT",
 )
