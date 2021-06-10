1bdfecb482792bdfffa4fb545ab9129f200a8c5c
Thomas Dahl Heshe <tdh@veo.co>
2021-06-10 10:06:44 +0200

    initial commit with cookiecutter project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Recreating colors in broken images using Kornia framework',
+    author='Thomas Heshe, Asger Græsholt and Morten Wehlast',
+    license='MIT',
+)
