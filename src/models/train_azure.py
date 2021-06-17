0b9795c06cdd25893e5399705e40c6e038bd86b8
Morten Wehlast Jørgensen <mortenwehlast@berkeley.edu>
2021-06-17 13:53:11 +0200

    added properties to azure

--- src/models/train_azure.py
+++ src/models/train_azure.py
@@ -195,13 +195,13 @@ class Trainer:
                 model_path=os.path.join(self.ROOT, "models", model_file),
                 model_name=self.args.model_name,
                 tags={"Training context": "Inline Training"},
-                # properties={
-                #    'LR': run.get_metrics()['LR'],
-                #    'Epochs': run.get_metrics()['Epochs'],
-                #    'Latent dim': run.get_metrics()['Latent dim'],
-                #    'Hidden dim': run.get_metrics()['Hidden dim'],
-                #    'Overall loss': run.get_metrics()['Overall loss'],
-                # }
+                properties={
+                    'LR': run.get_metrics()['LR'],
+                    'Epochs': run.get_metrics()['Epochs'],
+                    'Latent dim': run.get_metrics()['Latent dim'],
+                    'Hidden dim': run.get_metrics()['Hidden dim'],
+                    'Overall loss': run.get_metrics()['Overall loss'],
+                 }
             )
         else:
             run.complete()
