e808228b4fd49a22c66dfed1efe6ef693bf345cb
Morten Wehlast Jørgensen <mortenwehlast@berkeley.edu>
2021-06-22 15:19:21 +0200

    bug fix

--- src/azure/train_and_register_model.py
+++ src/azure/train_and_register_model.py
@@ -53,8 +53,8 @@ image_data = ws.datasets.get(args.data_name)
 # Create a script config
 script_config = ScriptRunConfig(
     source_directory=ROOT,
-    script=os.path.join(ROOT, "src", "models", "train_lightning.py"),
-    arguments=["--input-data", image_data.as_named_input("image_resto").as_mount()],
+    script=os.path.join(ROOT, "src", "models", "train.py"),
+    arguments=["input-data", image_data.as_named_input("image_resto").as_mount()],
     environment=project_env,
 )  # Use the environment created previously
 
