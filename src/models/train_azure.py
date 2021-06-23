7294c8064ba0db4cf1509d383db45795a1c0a580
Morten Wehlast Jørgensen <mortenwehlast@berkeley.edu>
2021-06-23 12:29:00 +0200

    removed bad import

--- src/models/train_azure.py
+++ src/models/train_azure.py
@@ -26,7 +26,6 @@ import wandb
 import pytorch_lightning as pl
 import optuna
 from src.models.model_lightning import ConvVAE, LoggingCallback
-from src.models.model_FC import Decoder, Encoder, Net
 
 log = logging.getLogger(__name__)
 
